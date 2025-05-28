bc5acbcaae217e8f32a81340055bf0a77a0952c8
yohaneYuri <164520341+yohaneYuri@users.noreply.github.com>
2025-05-28 18:49:55 +0800

    dependency fix and gitignore update

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
               cabal-install
               (haskellPackages.ghcWithPackages (p:
                 with p; [
-                  flex
+                  alex
                   happy
                 ]))            
               ];
