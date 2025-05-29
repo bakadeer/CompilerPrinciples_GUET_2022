2959e22d87b7332835043418595a0621f9223c69
yohaneYuri <164520341+yohaneYuri@users.noreply.github.com>
2025-05-29 23:29:02 +0800

    lexer, parser and ast

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import Compiler (compile)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  src <- getContents
+  print $ compile src False
