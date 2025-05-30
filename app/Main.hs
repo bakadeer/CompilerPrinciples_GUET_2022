304df172f90211ac7da2ccc79bf768435b7723df
luckycyang <kfove.liang@protonmail.com>
2025-05-30 14:27:48 +0800

    fixup file structure

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,5 @@
 module Main where
 
-{-
-import qualified Calc (main)
-import qualified CppLexer (main)
-import qualified GCContent (main)
-
-processInput :: String -> IO ()
-processInput "task1.1" = GCContent.main
-processInput "task1.2" = CppLexer.main
-processInput "task1.3" = Calc.main
-processInput _ = putStrLn "the task is not support"
-
-main :: IO ()
-main = do
-  readFile "meta/task.info" >>= putStr
-  input <- getLine
-  processInput input
--}
 import Compiler (compile)
 
 main :: IO ()
--- lib/Calc.hs
+++
@@ -1,9 +0,0 @@
-module Calc (main) where
-
-import Calc.Parser (parseCalc)
-
-main :: IO ()
-main = do
-  input <- getLine
-  let result = parseCalc input
-  print result
